a29fcf2801ac6e33c9f2399aa1411bd5ffe01c2c
Martin Mueller <damadden88@googlemail.com>
2021-01-12 12:22:30 +0100

    feat: cleanup

--- misc/init.sh
+++ misc/init.sh
@@ -24,10 +24,6 @@ aws --version
 createAccount default ${AWS_ACCESS_KEY_ID} ${AWS_SECRET_ACCESS_KEY}
 
 sudo apt-get remove docker docker-engine docker.io containerd runc -y
-# sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-
 
 curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker ubuntu
@@ -39,27 +35,7 @@ sudo apt-get remove python-configparser
 sudo pip3 -v install docker-compose
 
 # Install Tools
-# sudo apt update -y
 sudo apt install git htop iftop mc wget nodejs npm jq ruby -y
-# sudo snap install ruby --channel=2.5/stable --classic
-
-### Install AWS Cli v2
-#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-# curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
-# unzip awscliv2.zip
-# sudo ./aws/install
-# sudo apt install python3-pip -y
-#python3 -m pip install aws-cli
-# pip3 install docker-compose
-
-#  apt-get install awscli -y
-
-# createAccount wtf ${AWS_ACCESS_KEY_ID_WTF} ${AWS_SECRET_ACCESS_KEY_WTF}
-# createAccount dev ${AWS_ACCESS_KEY_ID_DEV} ${AWS_SECRET_ACCESS_KEY_BUILD_DEV}
-# createAccount qa ${AWS_ACCESS_KEY_ID_QA} ${AWS_SECRET_ACCESS_KEY_QA}
-# createAccount prod ${AWS_ACCESS_KEY_ID_PROD} ${AWS_SECRET_ACCESS_KEY_PROD}
-
-# cat ~/.aws/credentials
 
 wget https://aws-codedeploy-eu-central-1.s3.eu-central-1.amazonaws.com/latest/install
 chmod +x ./install
