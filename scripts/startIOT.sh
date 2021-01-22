520a0d60f0bd2f025bf558300a0db3d49156a067
Martin Mueller <damadden88@googlemail.com>
2021-01-22 07:10:05 +0100

    fix: bring in secret.env

--- scripts/startIOT.sh
+++ scripts/startIOT.sh
@@ -6,7 +6,7 @@ export STAGE="${DEPLOYMENT_GROUP_NAME:16}"
 
 # load secretes from AWS . If you don't want that you can comment that out and set the secret.env yourself
 rm -rf secret.env
-node scripts/secret-manager-to-env.js
+node /home/ubuntu/git/rasp4/scripts/secret-manager-to-env.js
 
 echo 'start IOT stuff'
 cd /home/ubuntu/git/rasp4
